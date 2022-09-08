faf48fefae0341f7e65c75b335e59cc3fb615018
Rifqixx <rifqi.farel@ui.ac.id>
2022-09-08 11:21:40 +0700

    Upload Lab 1 vers 2

--- wishlist/views.py
+++ wishlist/views.py
@@ -3,10 +3,9 @@ from wishlist.models import BarangWishlist
 
 # Create your views here.
 def show_wishlist(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Rifqi Farel Muhammad'
+    }
     return render(request, "wishlist.html", context)
-
-data_barang_wishlist = BarangWishlist.objects.all()
-context = {
-    'list_barang': data_barang_wishlist,
-    'nama': 'Rifqi Farel Muhammad'
-}
