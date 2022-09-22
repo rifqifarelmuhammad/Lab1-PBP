982140cdc232779b4fc15c47eafe33ad76688a7e
rifqifarelmuhammad <rifqifarel222@gmail.com>
2022-09-22 15:18:10 +0700

    Submit Lab 3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,6 +4,9 @@ from wishlist.views import return_data_XML
 from wishlist.views import return_data_JSON
 from wishlist.views import return_data_XML_by_id
 from wishlist.views import return_data_JSON_by_id
+from wishlist.views import register
+from wishlist.views import login_user
+from wishlist.views import logout_user
 
 app_name = 'wishlist'
 
@@ -13,4 +16,7 @@ urlpatterns = [
     path('json/', return_data_JSON, name='return_data_JSON'),
     path('xml/<int:id>', return_data_XML_by_id, name='return_data_XML_by_id'),
     path('json/<int:id>', return_data_JSON_by_id, name='return_data_JSON_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
